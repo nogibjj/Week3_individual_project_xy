224d1ec42105de7718523131e43281095c39987d
xyli1233 <123574025+xyli1233@users.noreply.github.com>
2023-02-05 22:30:27 +0000

    week3

--- snake/target/debug/deps/snake-71868ba7f41b0cbf.d
+++ snake/target/debug/deps/snake-71868ba7f41b0cbf.d
@@ -1,8 +1,9 @@
-/workspaces/Week3_individual_project_xy/snake/target/debug/deps/snake-71868ba7f41b0cbf.rmeta: src/main.rs src/draw.rs
+/workspaces/Week3_individual_project_xy/snake/target/debug/deps/snake-71868ba7f41b0cbf.rmeta: src/main.rs src/draw.rs src/snake.rs
 
-/workspaces/Week3_individual_project_xy/snake/target/debug/deps/snake-71868ba7f41b0cbf.d: src/main.rs src/draw.rs
+/workspaces/Week3_individual_project_xy/snake/target/debug/deps/snake-71868ba7f41b0cbf.d: src/main.rs src/draw.rs src/snake.rs
 
 src/main.rs:
 src/draw.rs:
+src/snake.rs:
 
 # env-dep:CLIPPY_ARGS=
--- snake/target/debug/deps/snake-90ef24dcdb25c20c.d
+++ snake/target/debug/deps/snake-90ef24dcdb25c20c.d
@@ -1,8 +1,9 @@
-/workspaces/Week3_individual_project_xy/snake/target/debug/deps/snake-90ef24dcdb25c20c.rmeta: src/main.rs src/draw.rs
+/workspaces/Week3_individual_project_xy/snake/target/debug/deps/snake-90ef24dcdb25c20c.rmeta: src/main.rs src/draw.rs src/snake.rs
 
-/workspaces/Week3_individual_project_xy/snake/target/debug/deps/snake-90ef24dcdb25c20c.d: src/main.rs src/draw.rs
+/workspaces/Week3_individual_project_xy/snake/target/debug/deps/snake-90ef24dcdb25c20c.d: src/main.rs src/draw.rs src/snake.rs
 
 src/main.rs:
 src/draw.rs:
+src/snake.rs:
 
 # env-dep:CLIPPY_ARGS=
